38a221a515dcf00554fb75ebe2aafef63b4f8b5b
luisguilhermepelzl <115004874+luisguilhermepelzl@users.noreply.github.com>
2024-03-24 21:54:57 -0400

    correcao-scrapyspider

--- brasileirao_dados/brasileirao_dados/spiders/brasileirao_dados.py
+++ brasileirao_dados/brasileirao_dados/spiders/brasileirao_dados.py
@@ -1,56 +1,3 @@
-# import scrapy
-#
-#
-# class BrasileiraoDadosSpider(scrapy.Spider):
-#     name = "brasileirao_dados"
-#
-#     anos_links = [
-#         {"ANO": 2012,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2012"},
-#         {"ANO": 2013,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2013"},
-#         {"ANO": 2014,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2014"},
-#         {"ANO": 2015,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2015"},
-#         {"ANO": 2016,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2016"},
-#         {"ANO": 2017,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2017"},
-#         {"ANO": 2018,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2018"},
-#         {"ANO": 2019,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2019"},
-#         {"ANO": 2020,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2020"},
-#         {"ANO": 2021,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2021"},
-#         {"ANO": 2022,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2022"},
-#         {"ANO": 2023,
-#          "LINK": "https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2023"}
-#     ]
-#
-#     def start_requests(self):
-#         for item in self.anos_links:
-#             yield scrapy.Request(url=item['LINK'], callback=self.parse, meta={'ano': item['ANO']})
-#
-#     def parse(self, response):
-#         ano = response.meta['ano']
-#         dados = response.css('.no-underline span::text').getall()
-#
-#         for i in range(0, len(dados), 3):
-#             mandante = dados[i].strip()
-#             visitante = dados[i + 1].strip()
-#             resultado = dados[i + 2].strip()
-#
-#             yield {
-#                 'ano': ano,
-#                 'mandante': mandante,
-#                 'visitante': visitante,
-#                 'resultado': resultado
-#             }
-
 import scrapy
 
 
@@ -97,7 +44,7 @@ class BrasileiraoDadosSpider(scrapy.Spider):
             mandante = dados[i].strip()
             visitante = dados[i + 1].strip()
             resultado = dados[i + 2].strip()
-            jogo_index = i // 3  # Índice do jogo dentro do ano
+            jogo_index = i // 3
 
             yield {
                 'ano': ano,
